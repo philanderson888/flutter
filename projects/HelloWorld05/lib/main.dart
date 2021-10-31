435ec66a2686ee18e66ac44c43ff00c4d8e880b4
philanderson888 <philanderson888@hotmail.com>
2021-10-31 19:20:31 +0000

    hello world 05

---
+++ projects/HelloWorld05/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Hello World',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Text('hello world from phil2'));
+  }
+}
