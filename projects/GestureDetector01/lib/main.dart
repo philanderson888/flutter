211e4720226d6c20f67593783b7411f121954de9
philanderson888 <philanderson888@hotmail.com>
2021-11-13 06:44:02 +0000

    detect gestures 01

---
+++ projects/GestureDetector01/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(title: 'Flutter Card List View Demo'),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+  final snackBar = const SnackBar(content: Text('Hello This Is A SnackBar'));
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+              child: Text(
+                index.toString()), 
+                onTap: () => {
+                  print('you clicked item index $index index $index')
+                },
+              );
+        },
+        padding: const EdgeInsets.all(100),
+        shrinkWrap: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
