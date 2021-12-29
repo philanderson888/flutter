76d24ba9f414d8e0d9f732b49796b13e5b1593e4
philanderson888 <philanderson888@hotmail.com>
2021-12-29 03:33:15 +0000

    add more forms and date picker

--- projects/Form05/lib/main.dart
+++ projects/Form05/lib/main.dart
@@ -1,189 +1,6 @@
-# forms
-
-## contents
-
-- [forms](#forms)
-  - [contents](#contents)
-  - [introduction](#introduction)
-  - [form with single field](#form-with-single-field)
-  - [textfield](#textfield)
-  - [textformfield](#textformfield)
-  - [form example - text and numbers](#form-example---text-and-numbers)
-  - [datetime picker](#datetime-picker)
-    - [install](#install)
-    - [use](#use)
-
-## introduction
-
-flutter forms can have inbuilt validation which is great from the coder and the user point of view
-
-## form with single field
-
-here is a sample [form](../projects/Form03) with just one field, but it does have validation
-
-```java
-import 'package:flutter/material.dart';
-void main() => runApp(const App());
-class App extends StatelessWidget {
-  const App({Key? key}): super(key:key);
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'An App',
-      home: Scaffold(
-        appBar:AppBar(title: const Text('An App')),
-        body: const Home(),
-      ),
-    );
-  }
-}
-
-class Home extends StatefulWidget{
-  const Home({Key? key}):super(key:key);
-  @override
-  State<Home> createState() => HomeState();
-}
-
-class HomeState extends State<Home>{
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context){
-    String validateEmail(String? value) {
-      String pattern =
-          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-          r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-          r"{0,253}[a-zA-Z0-9])?)*$";
-      RegExp regex = RegExp(pattern);
-      if (value == null || value.isEmpty || !regex.hasMatch(value))
-        return 'Enter a valid email address';
-      else
-        return null;
-    }
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('This is a form'),
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            onPressed: () => print('pressed'),
-          ),
-          Form(
-            key: _key,
-            autovalidateMode: AutovalidateMode.always,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Email Basic Validation'
-                  ),
-                  validator: (String? value){
-                    if (value == null || value.isEmpty){
-                      return 'mandatory field';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Email Advanced Validation'
-                  ),
-                  validator: (value) => validateEmail(value),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () => {
-                      if(_key.currentState!.validate()){
-                        print('submit')
-                      } else {
-                        print('dont submit')
-                      }
-                    },
-                    child: const Text('Submit'),
-                  ),
-                ),
-              ]
-            ),
-          ),
-        ],
-      )
-    );
-  }
-}
-```
-
-
-## textfield
-
-see [TextField01](../projects/TextField01)
-
-```java
-TextField(
-  decoration: const InputDecoration(
-    border: OutlineInputBorder(),
-    hintText: 'Enter some text here'
-  ),
-  onChanged: (text) => print('You entered text $text'),
-),
-```
-
-for a combination of a text input field which updates a label see [TextField02](../projects/TextField02)
-
-```java
-String _inputText = '';
-void _updateTextLabel(inputText) {
-  setState(() {
-    _inputText = inputText;
-  });
-  print('text field has been updated to $_inputText');
-}
-TextField(
-  decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: 'Enter some text here'),
-  onChanged: (inputText) => {
-    _updateTextLabel(inputText)
-  }
-),
-const Text(
-  'This is a text field',
-),
-Text(
-  _inputText,
-),
-```
-
-## textformfield
-
-these fields are required when the form is going to be validated, otherwise just use simple [textfield](#textfield)
-
-When we display the form, sometimes we wish to display the index field as well as a read-only field but also colour it slightly to indicate clearly to the user that it's a different kind of field so if they do click on it to change it, they can understand clearly that it's a read only field
-
-```java
-Container(
-  width: 500,
-  child: TextFormField(
-    enabled: false,
-    initialValue: album.id.toString(),
-    decoration: const InputDecoration(
-      fillColor: Color(0xffb5e6e1),
-      filled: true,
-      border: OutlineInputBorder(),
-      hintText: 'album id'
-    ),
-  ),
-),
-```
-
-## form example - text and numbers
-
-here is a complete form example taken from [Form04](../projects/Form04) 
-
-```java
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 void main() => runApp(const App());
 class App extends StatelessWidget {
   const App({Key? key}): super(key:key);
@@ -210,6 +27,7 @@ class HomeState extends State<Home>{
   TextEditingController _controller01 = TextEditingController();
   TextEditingController _controller02 = TextEditingController();
   TextEditingController _textFieldController = TextEditingController();
+  TextEditingController _dateTimeController = TextEditingController();
 
   int _counter01 = 3;
   int _counter02 = 3;
@@ -220,6 +38,7 @@ class HomeState extends State<Home>{
     _controller01.text = _counter01.toString();
     _controller02.text = _counter02.toString();
     _textFieldController.text = 'some text';
+    _dateTimeController.text = '2000-09-20 14:30';
   }
 
   void incrementCounter01(){
@@ -415,6 +234,14 @@ class HomeState extends State<Home>{
                   ),
                 ]
               ),
+              DateTimePicker(
+                type: DateTimePickerType.dateTimeSeparate,
+                firstDate: DateTime(2021),
+                lastDate: DateTime(2021),
+                controller: _dateTimeController,
+                dateLabelText: 'Date',
+                timeLabelText: 'Time',
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
                 child: ElevatedButton(
@@ -429,24 +256,3 @@ class HomeState extends State<Home>{
     );
   }
 }
-```
-
-## datetime picker
-
-to add datetime picker we can use native code.  however in this example I have imported a library to save a whole load of work!
-
-### install
-
-```java
-flutter pub add date_time_picker
-/*
-dependencies:
-  date_time_picker: ^2.1.0
-*/
-```
-
-### use
-
-```java
-import 'package:date_time_picker/date_time_picker.dart';
-```
