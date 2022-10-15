34612cd193bbcc0533ee4f1ddde194f8015c590e
philanderson888 <philanderson888@hotmail.com>
2022-10-15 05:22:03 +0100

    create PatrolConversation class

---
+++ projects/Course05BusinessCard2/lib/models/patrol_conversation.dart
@@ -0,0 +1,12 @@
+class PatrolConversation {
+  String name = '';
+  String notes = '';
+
+  PatrolConversation({
+    required String name2,
+    required String notes2,
+  }) {
+    name = name2;
+    notes = notes2;
+  }
+}
