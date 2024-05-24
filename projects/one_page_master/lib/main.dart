210d4a8b8f39f021d441a9fb1735e8e747b39e31
philanderson888 <philanderson888@hotmail.com>
2024-05-24 22:59:25 +0200

    amend button click code for icon button

--- projects/one_page_master/lib/main.dart
+++ projects/one_page_master/lib/main.dart
@@ -1,24 +1,51 @@
+// counter app
+
+// icon button
+
+// amends text in header when button is clicked
+
 import 'package:flutter/material.dart';
+
 void main() => runApp(const App());
+
 class App extends StatelessWidget {
   const App({Key? key}): super(key:key);
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title:'An App',
-      home: Scaffold(
-        appBar:AppBar(title: const Text('An App')),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.add_alert),
-                  onPressed: () => print('pressed'),
-                ),
-              ],
-            )
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int num = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful App Starter Code - Counter ' + num.toString()),
+        backgroundColor: Colors.red,
+      ),
+      body: const Text('app body'),
+      floatingActionButton: RawMaterialButton(
+        fillColor: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50.0),
         ),
+        constraints: BoxConstraints.tightFor(
+          height: 50.0,width: 100.0,
+        ),
+        onPressed: () {
+          setState( () {
+            num++;
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
