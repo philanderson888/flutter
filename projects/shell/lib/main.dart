170b377da37aabadd03cc64d09a026ded3a14330
philanderson888 <philanderson888@hotmail.com>
2024-09-22 04:04:35 +0100

    add shell folder to run any project

---
+++ projects/shell/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, Key? key}) : super(key: key);
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: const [
+          Text('Hello World   '),
+          Text('   Hello World   '),
+          Text('   Hello World   '),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context) //
+                  .primaryTextTheme
+                  .headline6,
+            ),
+          ),
+          const Expanded(
+
+            child: Center(
+              child: Text('Hello, world!!!!!'),
+            ),
+          ),
+          const Expanded(
+
+            child: Center(
+              child: Text('Hello, world!!!!!'),
+            ),
+
+          ),
+          const Expanded(
+
+            child: Center(
+              child: Text('Hello, world!!!!!'),
+            ),
+
+          ),
+          Expanded(
+
+            child: Center(
+              child: Image.asset(
+                'images/mountain.jpg',
+                fit: BoxFit.cover,
+              ),
+
+            ),
+
+          ),
+          Expanded(
+
+            child: Center(
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Image.network('https://picsum.photos/250' ,
+                      fit: BoxFit.cover,
+                    )
+
+                )
+
+            ),
+
+          ),
+
+          Expanded(
+
+            child: Center(
+                child: Image.network('https://media3.giphy.com/media/3oz8xUK8V7suY7W9SE/giphy.gif' ,
+                  fit: BoxFit.cover,
+                )
+
+            ),
+
+          ),
+
+
+        ],
+
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'My app', // used by the OS task switcher
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
+    ),
+  );
+}
