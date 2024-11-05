12b13ba147da051e9362a86f2e20b6e6deda0d60
philanderson888 <philanderson888@hotmail.com>
2024-11-05 12:57:16 +0000

    update shell projects which are blanks for running future projects

--- projects/shell/lib/main.dart
+++ projects/shell/lib/main.dart
@@ -1,157 +1,356 @@
-// alert_02
-
-// #region import
 import 'package:flutter/material.dart';
 
-// #endregion
-// #region main
+/// Flutter code sample for [showTimePicker].
+
 void main() {
-  runApp(const App());
+  runApp(const ShowTimePickerApp());
 }
 
-// #endregion
-// #region App => MaterialApp => Home page
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-  static const String _title = 'Flutter';
+class ShowTimePickerApp extends StatefulWidget {
+  const ShowTimePickerApp({super.key});
+
+  @override
+  State<ShowTimePickerApp> createState() => _ShowTimePickerAppState();
+}
+
+class _ShowTimePickerAppState extends State<ShowTimePickerApp> {
+  ThemeMode themeMode = ThemeMode.dark;
+  bool useMaterial3 = true;
+
+  void setThemeMode(ThemeMode mode) {
+    setState(() {
+      themeMode = mode;
+    });
+  }
+
+  void setUseMaterial3(bool? value) {
+    setState(() {
+      useMaterial3 = value!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.light(useMaterial3: useMaterial3),
+      darkTheme: ThemeData.dark(useMaterial3: useMaterial3),
+      themeMode: themeMode,
+      home: TimePickerOptions(
+        themeMode: themeMode,
+        useMaterial3: useMaterial3,
+        setThemeMode: setThemeMode,
+        setUseMaterial3: setUseMaterial3,
       ),
-      home: const Home(title: _title),
     );
   }
 }
 
-// #endregion
-// #region Home state => _HomeState
-class Home extends StatefulWidget {
-  const Home({Key? key, required this.title}) : super(key: key);
-  final String title;
+class TimePickerOptions extends StatefulWidget {
+  const TimePickerOptions({
+    super.key,
+    required this.themeMode,
+    required this.useMaterial3,
+    required this.setThemeMode,
+    required this.setUseMaterial3,
+  });
+
+  final ThemeMode themeMode;
+  final bool useMaterial3;
+  final ValueChanged<ThemeMode> setThemeMode;
+  final ValueChanged<bool?> setUseMaterial3;
+
   @override
-  State<Home> createState() => _HomeState();
+  State<TimePickerOptions> createState() => _TimePickerOptionsState();
 }
 
-// #endregion
-// #region _HomeState extends state
-class _HomeState extends State<Home> {
-  // #region initialise variables
-  int _counter = 0;
-  bool _ok = false;
-  bool _cancel = false;
-  // #endregion
-  // #region incrementCounter()
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _TimePickerOptionsState extends State<TimePickerOptions> {
+  TimeOfDay? selectedTime;
+  TimePickerEntryMode entryMode = TimePickerEntryMode.dial;
+  Orientation? orientation;
+  TextDirection textDirection = TextDirection.ltr;
+  MaterialTapTargetSize tapTargetSize = MaterialTapTargetSize.padded;
+  bool use24HourTime = false;
+
+  void _entryModeChanged(TimePickerEntryMode? value) {
+    if (value != entryMode) {
+      setState(() {
+        entryMode = value!;
+      });
+    }
   }
 
-  void _registerAlertOK() {
-    setState(() {
-      _ok = true;
-      _cancel = false;
-      _counter++;
-    });
-    print('ok set to $_ok');
+  void _orientationChanged(Orientation? value) {
+    if (value != orientation) {
+      setState(() {
+        orientation = value;
+      });
+    }
   }
 
-  void _registerAlertCancel() {
-    setState(() {
-      _ok = false;
-      _cancel = true;
-      _counter++;
-    });
-    print('_cancel set to $_cancel');
+  void _textDirectionChanged(TextDirection? value) {
+    if (value != textDirection) {
+      setState(() {
+        textDirection = value!;
+      });
+    }
   }
 
-  // #endregion
-  // #region widget builder
+  void _tapTargetSizeChanged(MaterialTapTargetSize? value) {
+    if (value != tapTargetSize) {
+      setState(() {
+        tapTargetSize = value!;
+      });
+    }
+  }
+
+  void _use24HourTimeChanged(bool? value) {
+    if (value != use24HourTime) {
+      setState(() {
+        use24HourTime = value!;
+      });
+    }
+  }
+
+  void _themeModeChanged(ThemeMode? value) {
+    widget.setThemeMode(value!);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // #region app bar
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      // #endregion
-      // #region body
-      body: Center(
-        // #region column
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Material(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 350,
+                mainAxisSpacing: 4,
+                mainAxisExtent: 200,
+                crossAxisSpacing: 4,
+              ),
+              children: <Widget>[
+                EnumCard<TimePickerEntryMode>(
+                  choices: TimePickerEntryMode.values,
+                  value: entryMode,
+                  onChanged: _entryModeChanged,
+                ),
+                EnumCard<ThemeMode>(
+                  choices: ThemeMode.values,
+                  value: widget.themeMode,
+                  onChanged: _themeModeChanged,
+                ),
+                EnumCard<TextDirection>(
+                  choices: TextDirection.values,
+                  value: textDirection,
+                  onChanged: _textDirectionChanged,
+                ),
+                EnumCard<MaterialTapTargetSize>(
+                  choices: MaterialTapTargetSize.values,
+                  value: tapTargetSize,
+                  onChanged: _tapTargetSizeChanged,
+                ),
+                ChoiceCard<Orientation?>(
+                  choices: const <Orientation?>[...Orientation.values, null],
+                  value: orientation,
+                  title: '$Orientation',
+                  choiceLabels: <Orientation?, String>{
+                    for (final Orientation choice in Orientation.values)
+                      choice: choice.name,
+                    null: 'from MediaQuery',
+                  },
+                  onChanged: _orientationChanged,
+                ),
+                ChoiceCard<bool>(
+                  choices: const <bool>[false, true],
+                  value: use24HourTime,
+                  onChanged: _use24HourTimeChanged,
+                  title: 'Time Mode',
+                  choiceLabels: const <bool, String>{
+                    false: '12-hour am/pm time',
+                    true: '24-hour time',
+                  },
+                ),
+                ChoiceCard<bool>(
+                  choices: const <bool>[false, true],
+                  value: widget.useMaterial3,
+                  onChanged: widget.setUseMaterial3,
+                  title: 'Material Version',
+                  choiceLabels: const <bool, String>{
+                    false: 'Material 2',
+                    true: 'Material 3',
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  padding: const EdgeInsets.all(16.0),
-                  primary: Colors.orange,
-                  textStyle: const TextStyle(fontSize: 20)),
-              onPressed: () => {
-                _incrementCounter(),
-                showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('This is an alert'),
-                    content: const Text('This is an alert description.'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => {
-                          print('You pressed "cancel"'),
-                          _registerAlertCancel(),
-                          Navigator.pop(context, 'Cancel')
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ElevatedButton(
+                    child: const Text('Open time picker'),
+                    onPressed: () async {
+                      final TimeOfDay? time = await showTimePicker(
+                        context: context,
+                        initialTime: selectedTime ?? TimeOfDay.now(),
+                        initialEntryMode: entryMode,
+                        orientation: orientation,
+                        builder: (BuildContext context, Widget? child) {
+                          // We just wrap these environmental changes around the
+                          // child in this builder so that we can apply the
+                          // options selected above. In regular usage, this is
+                          // rarely necessary, because the default values are
+                          // usually used as-is.
+                          return Theme(
+                            data: Theme.of(context).copyWith(
+                              materialTapTargetSize: tapTargetSize,
+                            ),
+                            child: Directionality(
+                              textDirection: textDirection,
+                              child: MediaQuery(
+                                data: MediaQuery.of(context).copyWith(
+                                  alwaysUse24HourFormat: use24HourTime,
+                                ),
+                                child: child!,
+                              ),
+                            ),
+                          );
                         },
-                        child: const Text('Cancel'),
-                      ),
-                      TextButton(
-                        onPressed: () => {
-                          _registerAlertOK(),
-                          print('You pressed "OK"'),
-                          Navigator.pop(context, 'OK')
-                        },
-                        child: const Text('OK'),
-                      ),
-                    ],
+                      );
+                      setState(() {
+                        selectedTime = time;
+                      });
+                    },
                   ),
                 ),
-              },
-              child: const Text('Show Dialog'),
+                if (selectedTime != null)
+                  Text('Selected time: ${selectedTime!.format(context)}'),
+              ],
             ),
-            Visibility(
-              child: const Text(
-                'You pressed OK button - well done',
-              ),
-              visible: _ok,
-            ),
-            Visibility(
-              child: const Text(
-                'You pressed the cancel button this time to cancel the dialog',
-              ),
-              visible: _cancel,
-            ),
-          ],
-        ),
-        // #endregion
+          ),
+        ],
       ),
-      // #endregion
-      // #region floating action button
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      // #endregion
     );
   }
-// #endregion
 }
-// #endregion
+
+// This is a simple card that presents a set of radio buttons (inside of a
+// RadioSelection, defined below) for the user to select from.
+class ChoiceCard<T extends Object?> extends StatelessWidget {
+  const ChoiceCard({
+    super.key,
+    required this.value,
+    required this.choices,
+    required this.onChanged,
+    required this.choiceLabels,
+    required this.title,
+  });
+
+  final T value;
+  final Iterable<T> choices;
+  final Map<T, String> choiceLabels;
+  final String title;
+  final ValueChanged<T?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // If the card gets too small, let it scroll both directions.
+      child: SingleChildScrollView(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(title),
+                ),
+                for (final T choice in choices)
+                  RadioSelection<T>(
+                    value: choice,
+                    groupValue: value,
+                    onChanged: onChanged,
+                    child: Text(choiceLabels[choice]!),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// This aggregates a ChoiceCard so that it presents a set of radio buttons for
+// the allowed enum values for the user to select from.
+class EnumCard<T extends Enum> extends StatelessWidget {
+  const EnumCard({
+    super.key,
+    required this.value,
+    required this.choices,
+    required this.onChanged,
+  });
+
+  final T value;
+  final Iterable<T> choices;
+  final ValueChanged<T?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceCard<T>(
+        value: value,
+        choices: choices,
+        onChanged: onChanged,
+        choiceLabels: <T, String>{
+          for (final T choice in choices) choice: choice.name,
+        },
+        title: value.runtimeType.toString());
+  }
+}
+
+// A button that has a radio button on one side and a label child. Tapping on
+// the label or the radio button selects the item.
+class RadioSelection<T extends Object?> extends StatefulWidget {
+  const RadioSelection({
+    super.key,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+    required this.child,
+  });
+
+  final T value;
+  final T? groupValue;
+  final ValueChanged<T?> onChanged;
+  final Widget child;
+
+  @override
+  State<RadioSelection<T>> createState() => _RadioSelectionState<T>();
+}
+
+class _RadioSelectionState<T extends Object?> extends State<RadioSelection<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsetsDirectional.only(end: 8),
+          child: Radio<T>(
+            groupValue: widget.groupValue,
+            value: widget.value,
+            onChanged: widget.onChanged,
+          ),
+        ),
+        GestureDetector(
+            onTap: () => widget.onChanged(widget.value), child: widget.child),
+      ],
+    );
+  }
+}
--- projects/shellios/lib/main.dart
+++ projects/shellios/lib/main.dart
@@ -1,174 +1,70 @@
-// to be completed
-// ignore_for_file: prefer_final_fields
-
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
-/// Stateful [App] overrides [createState()] to create its own state
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class Post {
-  int id;
-  String title, body, userId;
-
-  // default constructor
-  Post()
-      : id = 0,
-        title = 'this is a title',
-        body = 'this is a body',
-        userId = 'this is a user id';
-
-  // constructor with values
-  Post.overloadedConstructor(this.id, this.title, this.body, this.userId);
-
-  Map<String, dynamic> toJson() => {
-    'title': title,
-    'body': body,
-    'userId': userId,
-  };
-}
-
-/// [_AppState] holds the actual state so extends [State<App>] and also has an overridden [build()] method
-class _AppState extends State<App> {
-  final url = "https://jsonplaceholder.typicode.com/posts";
-  final _postObject = {
-    'title': 'A Post',
-    'body': 'Post Data',
-    'userId': "1001",
-  };
-
-  var _postInstance = Post();
-
-  void _updatePostResponse(Map<String, dynamic> apiResponse) {
-    setState(() {
-      _postInstance.id = apiResponse['id'];
-    });
-  }
-
-  void _postData() async {
-    try {
-      final response =
-      await post(Uri.parse(url), body: jsonEncode(_postObject));
-      print('\n\npost data - response is \n\n');
-      Map<String, dynamic> apiResponse = jsonDecode(response.body);
-      print('api response body = $apiResponse');
-      _updatePostResponse(apiResponse);
-    } catch (error) {
-      print('error ' + error.toString());
-    }
-  }
-
-  void _putData() async {
-    try {
-      final response = await put((Uri.parse(url + '/1')), body: jsonEncode(_postInstance));
-      print('\n\npost data - response is \n\n');
-      Map<String, dynamic> apiResponse = jsonDecode(response.body);
-      print('api response body after put = $apiResponse');
-    } catch (error) {
-      print('error ' + error.toString());
-    }
-  }
-
-  void _deleteData() async {
-    try {
-      final response = await delete(Uri.parse(url + '/1'));
-      print('\n\ndelete data - response is \n\n');
-      Map<String, dynamic> apiResponse = jsonDecode(response.body);
-      print('api response body after delete = $apiResponse');
-    } catch (error) {
-      print('error ' + error.toString());
-    }
-  }
-
-  void _updateTitle(String title) {
-    setState(() {
-      _postInstance.title = title;
-      print('title = $title');
-    });
-  }
-
-  void _updateBody(String body) {
-    setState(() {
-      _postInstance.body = body;
-      print('body = $body');
-    });
-  }
-
-  void _updateUserId(String userId) {
-    setState(() {
-      _postInstance.userId = userId;
-      print('userId = $userId');
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: const Text('this is an app')),
-          body: ListView(
-            padding: const EdgeInsets.all(10),
-            shrinkWrap: true,
-            children: <Widget>[
-              ElevatedButton(
-                child: const Text('press to post API data'),
-                onPressed: () {
-                  _postData();
-                },
-              ),
-              ElevatedButton(
-                child: const Text('press to put API data'),
-                onPressed: () {
-                  _putData();
-                },
-              ),
-              ElevatedButton(
-                child: const Text('press to delete API data'),
-                onPressed: () {
-                  _deleteData();
-                },
-              ),
-              Text(
-                  'id : ${_postInstance.id.toString()}, title: ${_postInstance.title}, body: ${_postInstance.body}, userId: ${_postInstance.userId}'),
-              TextFormField(
-                  initialValue: _postInstance.title,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Title',
-                  ),
-                  onChanged: (title) {
-                    _updateTitle(title);
-                  }),
-              TextFormField(
-                  initialValue: _postInstance.body,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Body',
-                  ),
-                  onChanged: (body) {
-                    _updateBody(body);
-                  }),
-              TextFormField(
-                  initialValue: _postInstance.userId,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'UserId',
-                  ),
-                  onChanged: (userId) {
-                    _updateUserId(userId);
-                  }),
-            ],
-          )),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+          padding: const EdgeInsets.all(100),
+          shrinkWrap: true,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: const <Widget>[
+              Text('Row 1'),
+            ]),
+            const Card(child: ListTile(title: Text('List Tile'))),
+          ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
